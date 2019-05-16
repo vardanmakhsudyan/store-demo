4166bdd79fa8faf377d10c8757d3ac8e0fa0a951
Айк <ajk@macbook-pro-ajk.local>
2019-05-16 23:02:38 +0400

    Initial Commit

---
+++ Store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Store
+//
+//  Created by Айк on 16.05.2019.
+//  Copyright © 2019 Vardan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
