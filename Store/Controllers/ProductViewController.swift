3f12740a938e79b5292eeb5b3350d9d020f22600
Айк <ajk@macbook-pro-ajk.local>
2019-05-18 11:36:07 +0400

    Created StoreTableViewController

---
+++ Store/Controllers/ProductViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProductViewController.swift
+//  Store
+//
+//  Created by Айк on 16.05.2019.
+//  Copyright © 2019 Vardan. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
